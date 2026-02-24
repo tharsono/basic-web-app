943dee696c8d17d6ab086bb108a901c4b992d739
tharsono <tharsono@andrew.cmu.edu>
2026-02-24 14:45:15 +0000

    changed my name

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -22,7 +22,7 @@ describe("QueryProcessor", () => {
         const query = "What is your name?";
         const response: string = QueryProcessor(query);
         expect(response).toBe((
-            "Rohan"
+            "tharsono"
           ));
     });
 
@@ -30,8 +30,8 @@ describe("QueryProcessor", () => {
         const query = "What is your Andrew ID?";
         const response: string = QueryProcessor(query);
         expect(response).toBe((
-            "My Andrew ID is rohanh."
-          ));
+            "My Andrew ID is tharsono."
+        ));
     });
 
 });
